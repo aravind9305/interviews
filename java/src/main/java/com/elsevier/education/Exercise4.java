d32fe6fa12f8a3635d2ea13b5f84e0d505363078
aravind9305 <aravind.yed@gmail.com>
2016-10-24 21:20:46 -0400

    Update Exercise4.java
    Count is a shared variable  in a multithreaded environment. So we have to make resetcount and increment count methods as synchronized. but getcount method need not be synchronized as it is just reading the value of count and not modifying the value of count

--- java/src/main/java/com/elsevier/education/Exercise4.java
+++ java/src/main/java/com/elsevier/education/Exercise4.java
@@ -11,7 +11,7 @@ public class Exercise4 {
 		
 		private int count = 0;
 		
-		public int increment() {
+		public  synchronized int increment() {
 			return ++count;
 		}
 		
@@ -19,7 +19,7 @@ public class Exercise4 {
 			return count;
 		}
 		
-		public void resetCount() {
+		public synchronized void resetCount() {
 			count = 0;
 		}
 
